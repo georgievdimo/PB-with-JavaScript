149c75eeb84f8f59be73c4ff3f6ade5c67b0c23e
Dimo Georgiev <57615323+georgievdimo@users.noreply.github.com>
2019-11-20 23:47:47 +0200

    Add files via upload
    Напишете програма, която изважда име, фамилия, възраст и град от аргумента input и печата съобщение от следния вид: "You are <firstName> <lastName>, a <age>-years old person from <town>."

---
+++ ConcatenateData.js
@@ -0,0 +1,7 @@
+function solve(input){
+    let firstName = input.shift();
+    let lastName = input.shift();
+    let age = input.shift();
+    let town = input.shift();
+    console.log(`You are ${firstName} ${lastName}, a ${age}-years old person from ${town}.`)
+}
