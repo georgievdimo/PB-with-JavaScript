4ba7ad630ba3b7b4d3a1fa40ec32dcf9154176a4
Dimo Georgiev <57615323+georgievdimo@users.noreply.github.com>
2019-11-21 00:39:35 +0200

    Update ConcatenateData.js

--- ConcatenateData.js
+++ ConcatenateData.js
@@ -1,3 +1,5 @@
+//Напишете програма, която изважда име, фамилия, възраст и град от аргумента input и печата съобщение от следния вид: "You are <firstName> <lastName>, a <age>-years old person from <town>."
+
 function solve(input) {
     let firstName = input.shift();
     let lastName = input.shift();
