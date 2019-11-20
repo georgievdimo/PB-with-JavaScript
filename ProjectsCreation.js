bcdf06a3361072d7344a71cec8f49c28650f285d
Dimo Georgiev <57615323+georgievdimo@users.noreply.github.com>
2019-11-20 23:24:54 +0200

    Delete ProjectsCreation.js

--- ProjectsCreation.js
+++
@@ -1,8 +0,0 @@
-function ProjectsCreation(input){
-    let name = input.shift();
-    let countProjects = Number(input.shift());
-    let time = countProjects * 3;
-    console.log(`The architect ${name} will need ${time} hours to complete ${countProjects} project/s.`);
-}
-
-ProjectsCreation(["Dimo", "5"])
