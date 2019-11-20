41845413b20f12b842a2ffe34e5453a8e3d1e64f
Dimo Georgiev <57615323+georgievdimo@users.noreply.github.com>
2019-11-20 23:24:30 +0200

    Delete PetShop.js

--- PetShop.js
+++
@@ -1,6 +0,0 @@
-function PetShop(input){
-    let dogsCount = Number(input.shift());
-    let otherAnimals = Number(input.shift());
-    let totalSum = dogsCount * 2.50 + otherAnimals * 4;
-    console.log(`${totalSum.toFixed(2)} lv.`);
-}
