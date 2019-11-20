9e6160c33a30ef6d08b7ceec97698ef5666f3b43
Dimo Georgiev <57615323+georgievdimo@users.noreply.github.com>
2019-11-20 23:55:08 +0200

    Delete ConcatenateData.js

--- ConcatenateData.js
+++
@@ -1,7 +0,0 @@
-function solve(input){
-    let firstName = input.shift();
-    let lastName = input.shift();
-    let age = input.shift();
-    let town = input.shift();
-    console.log(`You are ${firstName} ${lastName}, a ${age}-years old person from ${town}.`)
-}
