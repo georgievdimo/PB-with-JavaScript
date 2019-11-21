baaef57e93371424c6bb155536d73badb4f4176a
Dimo Georgiev <57615323+georgievdimo@users.noreply.github.com>
2019-11-21 09:45:33 +0200

    Update GreetingByName.js

--- Simple Operations and Calculations/GreetingByName.js
+++ Simple Operations and Calculations/GreetingByName.js
@@ -1,3 +1,5 @@
+//Напишете програма, която изважда от аргумента input име на човек и отпечатва &quot;Hello, &lt;name&gt;!&quot;, където &lt;name&gt; е въведеното име от конзолата.
+
 function solve(input) {
     let name = input.shift();
     console.log(`Hello, ${name}!`);
