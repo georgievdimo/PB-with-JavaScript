27f6800b02e758678679ad0ece0918a3bb7fa08b
Dimo Georgiev <57615323+georgievdimo@users.noreply.github.com>
2019-11-21 09:54:22 +0200

    Update YardGreening.js

--- Simple Operations and Calculations/YardGreening.js
+++ Simple Operations and Calculations/YardGreening.js
@@ -6,5 +6,5 @@ function solve(input) {
     let discount = price * 0.18;
     let finalPrice = price - discount;
     console.log(`The final price is: ${finalPrice.toFixed(2)} lv.`);
-    console.log(`The discount is: ${discount.toFixed(2)} lv.`)
+    console.log(`The discount is: ${discount.toFixed(2)} lv.`);
 }
