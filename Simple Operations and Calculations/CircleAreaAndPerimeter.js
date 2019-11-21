e950e9fd2c9dcad89d8bf56544d54f8d499f14e6
Dimo Georgiev <57615323+georgievdimo@users.noreply.github.com>
2019-11-21 09:44:25 +0200

    Update CircleAreaAndPerimeter.js

--- Simple Operations and Calculations/CircleAreaAndPerimeter.js
+++ Simple Operations and Calculations/CircleAreaAndPerimeter.js
@@ -1,3 +1,5 @@
+//Напишете програма, която изважда число r от аргумента input и пресмята и отпечатва лицето и периметъра на кръг / окръжност с радиус r, като форматирате изхода до втория знак след десетичната запетая.
+
 function solve(input) {
     let r = Number(input.shift());
     let area = Math.PI * r * r;
@@ -5,4 +7,3 @@ function solve(input) {
     console.log(area.toFixed(2));
     console.log(perimeter.toFixed(2));
 }
-
