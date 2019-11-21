435c5ab187d4f8a267835e9adba62655ea9fd482
Dimo Georgiev <57615323+georgievdimo@users.noreply.github.com>
2019-11-21 09:48:55 +0200

    Update InchesToCentimeters.js

--- Simple Operations and Calculations/InchesToCentimeters.js
+++ Simple Operations and Calculations/InchesToCentimeters.js
@@ -1,3 +1,5 @@
+//Да се напише програма, която изважда цяло число от аргумента input и преобразува числото от инчове в сантиметри.
+
 function solve(input) {
     let inches = Number(input.shift());
     let centimeters = inches * 2.54;
