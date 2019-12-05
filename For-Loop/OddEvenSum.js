c4eb7f1fea6684190e94580ce63eae9c97bdcdc7
Dimo Georgiev <57615323+georgievdimo@users.noreply.github.com>
2019-12-05 17:28:15 +0200

    Create OddEvenSum.js

---
+++ For-Loop/OddEvenSum.js
@@ -0,0 +1,21 @@
+//Да се напише програма, която получава n-на брой цели числа и проверява, дали сумата от числата на четни позиции е равна на сумата на числата на нечетни позиции. При равенство да се отпечатат "Yes" и на нов ред "Sum = " + сумата; иначе да се отпечата "No" и на нов ред "Diff = " + разликата. Разликата се изчислява по абсолютна стойност.
+
+function solve(input) {
+  let end = +input.shift();
+  let oddSum = 0;
+  let evenSum = 0;
+  for (i = 1; i <= end; i++) {
+    let currentNumber = +input.shift();
+    if (i % 2 == 0) {
+      evenSum += currentNumber;
+    } else {
+      oddSum += currentNumber;
+    }
+  }
+  if (evenSum === oddSum) {
+    console.log(`Yes\nSum = ${evenSum}`);
+  } else {
+    let diff = Math.abs(oddSum - evenSum);
+    console.log(`No\nDiff = ${diff}`);
+  }
+}
