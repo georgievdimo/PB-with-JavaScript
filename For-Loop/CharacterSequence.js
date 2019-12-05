3a68026055b3680c588b22207c475cdeb9a8b856
Dimo Georgiev <57615323+georgievdimo@users.noreply.github.com>
2019-12-06 00:15:44 +0200

    Delete CharacterSequence.js

--- For-Loop/CharacterSequence.js
+++
@@ -1,9 +0,0 @@
-//Напишете програма, която чете текст(стринг), въведен от потребителя и печата всеки символ от текста на отделен ред.
-
-function solve(input) {
-  let string = input.shift();
-  let end = string.length;
-  for (let i = 0; i < end; i++) {
-    console.log(string[i]);
-  }
-}
