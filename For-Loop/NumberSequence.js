5dcdbd35cbd30746b7d6a148ac96f2887a1d86e7
Dimo Georgiev <57615323+georgievdimo@users.noreply.github.com>
2019-12-06 00:19:50 +0200

    Create NumberSequence.js

---
+++ For-Loop/NumberSequence.js
@@ -0,0 +1,18 @@
+//Напишете програма, която прочита n на брой числа. Принтирайте най-голямото и най-малкото число сред въведените.
+
+function solve(input) {
+  let n = +input.shift();
+  let min = Number.MAX_SAFE_INTEGER;
+  let max = Number.MIN_SAFE_INTEGER;
+  for (let i = 1; i <= n; i++) {
+    let currentNumber = +input.shift();
+    if (currentNumber > max) {
+      max = currentNumber;
+    }
+    if (currentNumber < min) {
+      min = currentNumber;
+    }
+  }
+  console.log(`Max number: ${max}`);
+  console.log(`Min number: ${min}`);
+}
