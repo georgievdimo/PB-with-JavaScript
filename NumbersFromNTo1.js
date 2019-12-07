d94ad97ce31fa12e911c63bb850514f8189a0d65
Dimo Georgiev <57615323+georgievdimo@users.noreply.github.com>
2019-12-07 18:20:03 +0200

    Update NumbersFromNTo1.js

--- NumbersFromNTo1.js
+++ NumbersFromNTo1.js
@@ -1,5 +1,6 @@
 function solve(input){
   let number = +input.shift();  
- for (let i = number; i >= 1; i--){
-   console.log(i);  }
+  for (let i = number; i >= 1; i--){
+      console.log(i);  
+     }
 }
