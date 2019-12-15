8495c76e06fe6e2cb0098667be526f180eab7f66
Dimo Georgiev <57615323+georgievdimo@users.noreply.github.com>
2019-12-15 20:13:17 +0200

    Update ExamProblems.js

--- ExamProblems.js
+++ ExamProblems.js
@@ -1,1 +1,13 @@
+//
 
+function solve(input) {
+  let hallLenght = +input.shift();
+  let hallWight = +input.shift();
+  let barSide = +input.shift();
+  let hallArea = hallLenght * hallWight;
+  let barArea = barSide * barSide;
+  let danceArea = hallArea * 0.19;
+  let freeSpace = hallArea - barArea - danceArea;
+  let guest = freeSpace / 3.2;
+  console.log(Math.ceil(guest));
+}
